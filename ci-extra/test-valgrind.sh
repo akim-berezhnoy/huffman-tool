d3de148fe90914fd4dade7fb78365b716f0a168e
Ivan Sorokin <vanyacpp@gmail.com>
2021-05-05 10:40:44 +0300

    add --show-leak-kinds=all to valgrind parameters
    This option allows seeing "still reachable" leaks.

--- ci-extra/test-valgrind.sh
+++ ci-extra/test-valgrind.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-valgrind --tool=memcheck --gen-suppressions=all --leak-check=full --leak-resolution=med --track-origins=yes --vgdb=no --error-exitcode=1 cmake-build-RelWithDebInfo/main
+valgrind --tool=memcheck --gen-suppressions=all --leak-check=full --show-leak-kinds=all --leak-resolution=med --track-origins=yes --vgdb=no --error-exitcode=1 cmake-build-RelWithDebInfo/main
