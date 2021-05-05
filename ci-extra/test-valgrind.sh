c9565a02e84e6bbf670482ff86ef2396644463e8
Ivan Sorokin <vanyacpp@gmail.com>
2021-05-05 14:18:22 +0300

    fix d3de148fe90914fd4dade7fb78365b716f0a168e
    Now the script can run with any current directory.

--- ci-extra/test-valgrind.sh
+++ ci-extra/test-valgrind.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-valgrind --tool=memcheck --gen-suppressions=all --leak-check=full --show-leak-kinds=all --leak-resolution=med --track-origins=yes --vgdb=no --error-exitcode=1 cmake-build-RelWithDebInfo/main
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+valgrind --tool=memcheck --gen-suppressions=all --leak-check=full --show-leak-kinds=all --leak-resolution=med --track-origins=yes --vgdb=no --error-exitcode=1 --suppressions="${SCRIPT_DIR}/valgrind.suppressions" cmake-build-RelWithDebInfo/main
