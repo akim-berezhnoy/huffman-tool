42a821bacf20efbd0a23553373648d92ef5e3a7e
akim-berezhnoy <berezhnoy.akim@gmail.com>
2023-06-25 21:19:10 +0300

    applied Pavel B 0001-Fixing-pipeline.patch diff not by hands

--- ci-extra/build.sh
+++ ci-extra/build.sh
@@ -4,6 +4,6 @@ IFS=$' \t\n'
 
 mkdir -p cmake-build-$1
 rm -rf cmake-build-$1/*
-cmake "-DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake" -GNinja --preset $1 \
+cmake "-DCMAKE_C_COMPILER=$2" "-DCMAKE_CXX_COMPILER=$3" "-DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake" -GNinja --preset $1 \
   -DENABLE_SLOW_TEST=ON -DTREAT_WARNINGS_AS_ERRORS=ON -S .
 cmake --build cmake-build-$1
