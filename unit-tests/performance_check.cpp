8d1b1ab39400f716a6c977184ccc38d985ca224e
akim-berezhnoy <berezhnoy.akim@gmail.com>
2023-06-26 00:06:05 +0300

    lowered memory usage in perf test

--- unit-tests/performance_check.cpp
+++ unit-tests/performance_check.cpp
@@ -2,20 +2,20 @@
 #include "gtest/gtest.h"
 #include "ss.h"
 
-static constexpr size_t HUGE_SIZE = static_cast<size_t>(1) << 31;
+static constexpr size_t HUGE_SIZE = static_cast<size_t>(1) << 30;
 
 ss h_initial(HUGE_SIZE);
 ss h_encoded;
 ss h_decoded;
 
-TEST(performance_check, stream_encode_2GB) {
+TEST(performance_check, stream_encode_1GB) {
   encode(h_initial, h_encoded);
 }
 
-TEST(performance_check, stream_decode_2GB) {
+TEST(performance_check, stream_decode_1GB) {
   decode(h_encoded, h_decoded);
 }
 
-TEST(performance_check, stream_correctness_check_2GB) {
+TEST(performance_check, stream_correctness_check_1GB) {
   ASSERT_EQ(h_initial, h_decoded);
 }
